e0b3004de954804882ffd82dda0d8a20d96c1b37
haowang21 <haowang21@iflytek.com>
2018-06-12 11:22:32 +0800

    11

--- src/test/java/com/rcloud/sample/RcloudSpringbootSampleApplicationTests.java
+++ src/test/java/com/rcloud/sample/RcloudSpringbootSampleApplicationTests.java
@@ -1,18 +1,11 @@
 package com.rcloud.sample;
 
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.file.Paths;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import io.github.swagger2markup.Swagger2MarkupConfig;
-import io.github.swagger2markup.Swagger2MarkupConverter;
-import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
-import io.github.swagger2markup.markup.builder.MarkupLanguage;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
